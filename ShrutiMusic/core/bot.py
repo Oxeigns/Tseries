5f6c22d7ed9aefe7906bbcb15b46354f622a08c0
Samrat <yashsamrat10080@gmail.com>
2025-06-25 17:40:40 +0530

    Update bot.py

--- ShrutiMusic/core/bot.py
+++ ShrutiMusic/core/bot.py
@@ -16,7 +16,7 @@ class Aviax(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="˹ᴛʜᴇ sɪʀɪᴏɴ ʙᴏᴛ˼",
+            name="𝐓 - 𝐒𝐄𝐑𝐈𝐄𝐒 𝐌𝐔𝐒𝐈𝐂",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
